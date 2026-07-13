d1c732ad81a44a89d59730c7a40b3305c807a84a
dakshvasani <dakshvasani@gmail.com>
2026-07-13 13:35:19 +0530

    Added JWT protected routes and authentication guard

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,13 +1,9 @@
 import { Navigate } from "react-router-dom";
 
-function ProtectedRoute({ children }) {
+function PrivateRoute({ children }) {
   const token = localStorage.getItem("access");
 
-  return token ? (
-    children
-  ) : (
-    <Navigate to="/login" />
-  );
+  return token ? children : <Navigate to="/" />;
 }
 
-export default ProtectedRoute;
+export default PrivateRoute;
